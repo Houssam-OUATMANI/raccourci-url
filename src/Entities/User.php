86313da5119cebe0407e5bc479ff190b06bd2888
Houssam OUATMANI <ouatmanihoussam@gmail.com>
2026-06-04 17:03:19 +0200

    Add User entity with properties and setter methods

---
+++ src/Entities/User.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Entities;
+
+
+class User
+{
+
+    public ?int $id;
+    public string $username;
+    public string $email;
+    public string $password;
+    public string $role;
+
+
+    public function set_id(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+
+    public function set_username(string $username): self
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+
+    public function set_email(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+
+    public function set_password(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+}
